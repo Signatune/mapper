e6a377e59a37d55dcb2e4ff0f8aae43007991cb2
Cayal Walker <cayalw@gmail.com>
2022-09-13 18:55:34 -0400

    Resolves createRoot deprecation error

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,5 @@
-import ReactDOM from "react-dom";
-import { App } from "./App";
-
-const app = document.getElementById("app");
-ReactDOM.render(<App />, app);
+import { App } from './App';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App />);
